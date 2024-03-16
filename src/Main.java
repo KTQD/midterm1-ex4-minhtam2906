822d9591491ed1142473f0e543abfa3da2ca145a
Nguyễn Minh Tâm <157863014+minhtam2906@users.noreply.github.com>
2024-03-16 14:50:40 +0700

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,28 @@
-public class Main {
+public class MultiThreadExample {
+    private static int[] array = {1, 3, 5, 6, 2, 7, 8, 0, 4, 3, 9, 2, 8, 1, 0, 5, 7, 4, 6, 9, 3, 2, 1, 8, 4, 0, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0}; // Mảng số nguyên đã cho
+
     public static void main(String[] args) {
-        int[] numbers = {1, 3, 5, 6, 2, 7, 8, 0, 4, 3, 9, 2, 8, 1, 0, 5, 7, 4, 6, 9, 3, 2, 1, 8, 4, 0, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0};
-    
+        // Khởi tạo và bắt đầu các luồng
+        Thread sumThread = new Thread(() -> {
+            int sum = 0;
+            for (int num : array) {
+                sum += num;
+            }
+            System.out.println("Tổng các phần tử trong mảng: " + sum);
+        });
 
+        Thread maxThread = new Thread(() -> {
+            int max = array[0];
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] > max) {
+                    max = array[i];
+                }
+            }
+            System.out.println("Phần tử lớn nhất trong mảng: " + max);
+        });
 
-        Viết mã của bạn vào đây
-    
+        // Bắt đầu các luồng
+        sumThread.start();
+        maxThread.start();
     }
 }
